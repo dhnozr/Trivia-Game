ba2ac6752820e549818e4c4067da56424a9e8b68
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-17 15:26:14 +0300

    routes are added

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ import {
 
 import Layout from './Layout';
 import HomePage from './HomePage';
+import Quiz from './Quiz';
 
 const route = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<HomePage />} />
+      <Route path="quiz" element={<Quiz />} />
     </Route>
   )
 );
