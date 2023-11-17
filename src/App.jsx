65d10b61b1372498ffcdec1873a1b283c1b721f2
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2023-11-17 09:15:17 +0300

    Routes are defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import { useState } from 'react';
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
 
-import './App.css';
+import Layout from './Layout';
+import HomePage from './HomePage';
+
+const route = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+);
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={route}></RouterProvider>
+    </>
+  );
 }
 
 export default App;
